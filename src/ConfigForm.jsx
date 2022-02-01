5f6eb1742ede9afa93f3b4b87bd7f3144d396a68
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2022-02-01 18:37:18 +0100

    Add edition buttons

--- src/ConfigForm.jsx
+++ src/ConfigForm.jsx
@@ -2,12 +2,18 @@
 
 import {
   Button,
+  ButtonGroup,
   Box,
   Card,
   CardActionArea,
   CardContent,
   Typography,
 } from '@mui/material';
+import {
+  HighlightOff,
+  ContentCopy,
+  Edit,
+} from '@mui/icons-material';
 
 const Wrapper = ({ edit, href, ...props }) => {
   if (edit) {
@@ -34,37 +40,59 @@ const ConfigForm = ({
   } = { data: {} },
   onDelete = () => null,
   onClone = () => null,
-}) => (
-  <Card
-    sx={{
-      flex: 1,
-      backgroundImage: `url(${picture})`,
-      backgroundPosition: 'center center',
-      backgroundSize: 'cover',
-      display: 'flex',
-    }}
-  >
-    <Wrapper edit={edit} href={lien} target="_blank">
-      <CardContent>
-        <Typography gutterBottom variant="h4" component="h2">
-          {titre}
-        </Typography>
+}) => {
+  const handleEdit = () => {};
 
-        {description && (
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-        )}
+  return (
+    <Card
+      sx={{
+        flex: 1,
+        backgroundImage: `url(${picture})`,
+        backgroundPosition: 'center center',
+        backgroundSize: 'cover',
+        display: 'flex',
+      }}
+    >
+      <Wrapper edit={edit} href={lien} target="_blank">
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="h2">
+            {titre}
+          </Typography>
 
-        {Boolean(edit) && (
-          <>
-            <Button onClick={event => { onDelete(key); event.stopPropagation(); }}>delete</Button>
-            <Button onClick={event => { onClone(key); event.stopPropagation(); }}>clone</Button>
-          </>
-        )}
-      </CardContent>
-    </Wrapper>
-  </Card>
-);
+          {description && (
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+          )}
+
+          {Boolean(edit) && (
+            <ButtonGroup sx={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', bgcolor: 'white' }}>
+              <Button
+                onClick={() => onDelete(key)}
+                color="error"
+              >
+                <HighlightOff />
+              </Button>
+
+              <Button
+                onClick={() => onClone(key)}
+                color="success"
+              >
+                <ContentCopy />
+              </Button>
+
+              <Button
+                onClick={handleEdit}
+                color="info"
+              >
+                <Edit />
+              </Button>
+            </ButtonGroup>
+          )}
+        </CardContent>
+      </Wrapper>
+    </Card>
+  );
+};
 
 export default ConfigForm;
